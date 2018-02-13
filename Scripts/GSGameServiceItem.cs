fecd6f14a3ab53b16fba761d46f77a4f60b9162e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-13 12:51:22 +0700

    Implement item services

--- Scripts/GSGameServiceItem.cs
+++ Scripts/GSGameServiceItem.cs
@@ -2,37 +2,216 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using GameSparks.Core;
 
 public partial class GSGameService
 {
     protected override void DoLevelUpItem(string playerId, string loginToken, string itemId, Dictionary<string, int> materials, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        data.AddString("itemId", itemId);
+        var materialsObject = new GSRequestData();
+        foreach (var material in materials)
+        {
+            materialsObject.AddNumber(material.Key, material.Value);
+        }
+        data.AddObject("materials", materialsObject);
+        var request = GetGSEventRequest("LevelUpItem", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+                    var deleteItemIds = scriptData.GetStringList("deleteItemIds");
+                    var updateCurrencies = scriptData.GetGSDataList("updateCurrencies");
+                    
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    result.deleteItemIds.AddRange(deleteItemIds);
+                    foreach (var entry in updateCurrencies)
+                    {
+                        var resultEntry = new PlayerCurrency();
+                        PlayerCurrency.CloneTo(JsonUtility.FromJson<DbPlayerCurrency>(entry.JSON), resultEntry);
+                        result.updateCurrencies.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoEvolveItem(string playerId, string loginToken, string itemId, Dictionary<string, int> materials, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        data.AddString("itemId", itemId);
+        var materialsObject = new GSRequestData();
+        foreach (var material in materials)
+        {
+            materialsObject.AddNumber(material.Key, material.Value);
+        }
+        data.AddObject("materials", materialsObject);
+        var request = GetGSEventRequest("EvolveItem", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+                    var deleteItemIds = scriptData.GetStringList("deleteItemIds");
+                    var updateCurrencies = scriptData.GetGSDataList("updateCurrencies");
+
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    result.deleteItemIds.AddRange(deleteItemIds);
+                    foreach (var entry in updateCurrencies)
+                    {
+                        var resultEntry = new PlayerCurrency();
+                        PlayerCurrency.CloneTo(JsonUtility.FromJson<DbPlayerCurrency>(entry.JSON), resultEntry);
+                        result.updateCurrencies.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoSellItems(string playerId, string loginToken, Dictionary<string, int> items, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        var itemsObject = new GSRequestData();
+        foreach (var item in items)
+        {
+            itemsObject.AddNumber(item.Key, item.Value);
+        }
+        data.AddObject("items", itemsObject);
+        var request = GetGSEventRequest("SellItems", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+                    var deleteItemIds = scriptData.GetStringList("deleteItemIds");
+                    var updateCurrencies = scriptData.GetGSDataList("updateCurrencies");
+
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    result.deleteItemIds.AddRange(deleteItemIds);
+                    foreach (var entry in updateCurrencies)
+                    {
+                        var resultEntry = new PlayerCurrency();
+                        PlayerCurrency.CloneTo(JsonUtility.FromJson<DbPlayerCurrency>(entry.JSON), resultEntry);
+                        result.updateCurrencies.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoEquipItem(string playerId, string loginToken, string characterId, string equipmentId, string equipPosition, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        var itemsObject = new GSRequestData();
+        data.AddString("characterId", characterId);
+        data.AddString("equipmentId", equipmentId);
+        data.AddString("equipPosition", equipPosition);
+        var request = GetGSEventRequest("EquipItem", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoUnEquipItem(string playerId, string loginToken, string equipmentId, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        var itemsObject = new GSRequestData();
+        data.AddString("equipmentId", equipmentId);
+        var request = GetGSEventRequest("UnEquipItem", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoGetAvailableLootBoxList(UnityAction<AvailableLootBoxListResult> onFinish)
@@ -44,6 +223,49 @@ public partial class GSGameService
     protected override void DoOpenLootBox(string playerId, string loginToken, string lootBoxDataId, int packIndex, UnityAction<ItemResult> onFinish)
     {
         var result = new ItemResult();
-        onFinish(result);
+        var data = new GSRequestData();
+        data.AddString("lootBoxDataId", lootBoxDataId);
+        data.AddNumber("packIndex", packIndex);
+        var request = GetGSEventRequest("OpenLootBox", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var createItems = scriptData.GetGSDataList("createItems");
+                    var updateItems = scriptData.GetGSDataList("updateItems");
+                    var deleteItemIds = scriptData.GetStringList("deleteItemIds");
+                    var updateCurrencies = scriptData.GetGSDataList("updateCurrencies");
+
+                    foreach (var entry in createItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.createItems.Add(resultEntry);
+                    }
+                    foreach (var entry in updateItems)
+                    {
+                        var resultEntry = new PlayerItem();
+                        PlayerItem.CloneTo(JsonUtility.FromJson<DbPlayerItem>(entry.JSON), resultEntry);
+                        result.updateItems.Add(resultEntry);
+                    }
+                    result.deleteItemIds.AddRange(deleteItemIds);
+                    foreach (var entry in updateCurrencies)
+                    {
+                        var resultEntry = new PlayerCurrency();
+                        PlayerCurrency.CloneTo(JsonUtility.FromJson<DbPlayerCurrency>(entry.JSON), resultEntry);
+                        result.updateCurrencies.Add(resultEntry);
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 }
