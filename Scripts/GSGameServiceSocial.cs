bb73f6b49526f9d2bbcdff5287cf3fd38a9fd020
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-15 22:03:01 +0700

    WIP Social system

---
+++ Scripts/GSGameServiceSocial.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public partial class GSGameService
+{
+    protected override void DoGetRandomPlayerList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
+    {
+
+    }
+
+    protected override void DoGetFriendList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
+    {
+
+    }
+
+    protected override void DoGetFriendRequestList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
+    {
+
+    }
+
+    protected override void DoFriendRequest(string playerId, string loginToken, string targetPlayerId, UnityAction<GameServiceResult> onFinish)
+    {
+
+    }
+
+    protected override void DoFriendAccept(string playerId, string loginToken, string targetPlayerId, UnityAction<FriendListResult> onFinish)
+    {
+
+    }
+
+    protected override void DoFriendDecline(string playerId, string loginToken, string targetPlayerId, UnityAction<GameServiceResult> onFinish)
+    {
+
+    }
+
+    protected override void DoFriendDelete(string playerId, string loginToken, string targetPlayerId, UnityAction<FriendListResult> onFinish)
+    {
+
+    }
+}
