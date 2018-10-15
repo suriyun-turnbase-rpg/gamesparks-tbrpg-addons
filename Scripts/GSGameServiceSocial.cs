bda5bfa2ca7bbc5727df1192acb7053b41f2b604
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-15 23:38:46 +0700

    WIP social services

--- Scripts/GSGameServiceSocial.cs
+++ Scripts/GSGameServiceSocial.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using GameSparks.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,36 +8,166 @@ public partial class GSGameService
 {
     protected override void DoGetHelperList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var request = GetGSEventRequest("GetHelperList");
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null && scriptData.ContainsKey("list"))
+            {
+                var list = scriptData.GetGSDataList("list");
+                foreach (var entry in list)
+                {
+                    result.list.Add(JsonUtility.FromJson<Player>(entry.JSON));
+                }
+                onFinish(result);
+            }
+        });
     }
 
     protected override void DoGetFriendList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var request = GetGSEventRequest("GetFriendList");
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null && scriptData.ContainsKey("list"))
+            {
+                var list = scriptData.GetGSDataList("list");
+                foreach (var entry in list)
+                {
+                    result.list.Add(JsonUtility.FromJson<Player>(entry.JSON));
+                }
+                onFinish(result);
+            }
+        });
     }
 
     protected override void DoGetFriendRequestList(string playerId, string loginToken, UnityAction<FriendListResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var request = GetGSEventRequest("GetFriendRequestList");
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null && scriptData.ContainsKey("list"))
+            {
+                var list = scriptData.GetGSDataList("list");
+                // TODO: Set list data
+                onFinish(result);
+            }
+        });
     }
 
     protected override void DoFriendRequest(string playerId, string loginToken, string targetPlayerId, UnityAction<GameServiceResult> onFinish)
     {
-
+        var result = new GameServiceResult();
+        var data = new GSRequestData();
+        data.AddString("targetPlayerId", targetPlayerId);
+        var request = GetGSEventRequest("FriendRequest", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoFriendAccept(string playerId, string loginToken, string targetPlayerId, UnityAction<FriendListResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var data = new GSRequestData();
+        data.AddString("targetPlayerId", targetPlayerId);
+        var request = GetGSEventRequest("FriendAccept", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var list = scriptData.GetGSDataList("list");
+                    foreach (var entry in list)
+                    {
+                        result.list.Add(JsonUtility.FromJson<Player>(entry.JSON));
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoFriendDecline(string playerId, string loginToken, string targetPlayerId, UnityAction<GameServiceResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var data = new GSRequestData();
+        data.AddString("targetPlayerId", targetPlayerId);
+        var request = GetGSEventRequest("FriendDecline", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var list = scriptData.GetGSDataList("list");
+                    foreach (var entry in list)
+                    {
+                        result.list.Add(JsonUtility.FromJson<Player>(entry.JSON));
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 
     protected override void DoFriendDelete(string playerId, string loginToken, string targetPlayerId, UnityAction<FriendListResult> onFinish)
     {
-
+        var result = new FriendListResult();
+        var data = new GSRequestData();
+        data.AddString("targetPlayerId", targetPlayerId);
+        var request = GetGSEventRequest("FriendDelete", data);
+        request.Send((response) =>
+        {
+            GSData scriptData = response.ScriptData;
+            if (scriptData != null)
+            {
+                if (scriptData.ContainsKey("error") && !string.IsNullOrEmpty(scriptData.GetString("error")))
+                {
+                    result.error = scriptData.GetString("error");
+                    onFinish(result);
+                }
+                else
+                {
+                    var list = scriptData.GetGSDataList("list");
+                    foreach (var entry in list)
+                    {
+                        result.list.Add(JsonUtility.FromJson<Player>(entry.JSON));
+                    }
+                    onFinish(result);
+                }
+            }
+        });
     }
 }
